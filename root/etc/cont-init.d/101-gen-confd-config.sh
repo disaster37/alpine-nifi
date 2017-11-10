60e5d2071a42873d82c5996386a2da2be25f5d9d
disaster37 <linuxworkgroup@hotmail.com>
2017-11-10 14:25:35 +0000

    Add log to stdout

--- root/etc/cont-init.d/101-gen-confd-config.sh
+++ root/etc/cont-init.d/101-gen-confd-config.sh
@@ -10,3 +10,14 @@ keys = [
   "/config"
 ]
 EOF
+
+cat << EOF > ${CONFD_HOME}/etc/conf.d/logback.xml.toml
+[template]
+prefix = "${CONFD_PREFIX_KEY}"
+src = "logback.xml.tmpl"
+dest = "/opt/nifi/conf/logback.xml"
+mode = "0744"
+keys = [
+  "/config"
+]
+EOF
